aba80f7af785062475736cd7a80f482e5a9ec10f
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-09-19 11:10:34 +0200

    Initial commit

---
+++ discord_logger/discord_logger.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+import time
+from typing import Literal
+
+import httpx
+
+from fastapi import (
+    FastAPI,
+    Request,
+)
+
+httpx_client = httpx.AsyncClient()
+
+
+def add_monitoring(app: FastAPI, logger_base_url: str, service_name: str):
+    @app.middleware("http")
+    async def logger(request: Request, call_next):
+        start_time = time.perf_counter()
+        response = await call_next(request)
+        end_time = time.perf_counter() - start_time
+        data = {
+            "service_name": service_name,
+            "route_name": str(request.url)[len(str(request.base_url)):].split("?")[0],
+            "request" : {
+                "timestamp": datetime.now(),
+                "response_time": end_time,
+            }
+        }
+        await httpx_client.post(
+            f"{logger_base_url}/monitoring/add-request",
+            data=data,
+        )
+        return response
+
+
+async def log(logger_base_url: str, channel_id: int, message: str, level: Literal["INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"):
+    data = {
+        "channel_id": channel_id,
+        "logging_message" : {
+            "message": message,
+            "level": level,
+        }
+    }
+    await httpx_client.post(
+        f"{logger_base_url}/monitoring/add-request",
+        data=data,
+    )
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="discord_logger",
+    version="0.1",
+    include_package_data=True,
+    python_requires='>=3.12',
+    packages=find_packages(),
+    setup_requires=['setuptools-git-versioning'],
+    install_requires=requirements,
+    author="Bixente Grandjean",
+    author_email="bixente.grandjean@gmail.com",
+    description="Functions to use with the DiscordLogger project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    version_config={
+       "dirty_template": "{tag}",
+    }
+)
